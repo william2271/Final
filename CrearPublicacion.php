3738f87cce125af489be37524faf30f88d065947
unknown <William22711@gmail.com>
2020-08-01 09:51:11 -0400

    Update CrearPublicacion.php

--- CrearPublicacion.php
+++ CrearPublicacion.php
@@ -7,7 +7,7 @@
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Nuevo Usuario</title>
+<title>Nuevo publicacion</title>
 <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
 
 </head>
